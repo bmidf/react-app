5b4d5645e5a15ebe236f7f8c8b078be831290457
bmidf <55makharadze@gmail.com>
2022-12-01 19:44:04 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Banner from "./components/Banner"
 import Banner2 from "./components/Banner2"
 import Banner3 from "./components/Banner3"
+import Banner4 from "./components/Banner4"
 import Footer from "./components/Footer"
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Banner/>
       <Banner2/>
       <Banner3/>
+      <Banner4/>
       <Footer/>
     </div>
   );
---
+++ src/components/Banner4.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import footerDog from '../assets/images/footerDog.png' 
+
+const Banner4 =()=>{
+    return (
+        <div className='banner4'>
+            <div className='flex'>
+                <div className='banner4_1'>
+                    <div className='footer_dog'>
+                        <img src={footerDog} className="footerDog" alt="sourceImage"/>
+                    </div>
+                </div>
+                <div className='banner4_2'>
+                    <div className='emailText'>
+                        <a>Subscribe & Get Pet Update and News</a>
+                    </div>
+                    <div className='inputMail'>
+                        <input className='inputMail2'type="text" name="name" placeholder='Mail' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner4
